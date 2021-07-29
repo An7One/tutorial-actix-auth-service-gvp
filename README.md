cf9414eb8126d93a0bd2c7ed32f6fd50fb9bf021
Leon <yanglyu.leon.7@gmail.com>
2021-07-28 17:28:49 -0700

    start: Building an Authentication Service Using Actix
    tutorial: setting up the application
    
    module 1

---
+++ README.md
@@ -0,0 +1,1 @@
+A literal follow-up with the tutorial, provided by <i>Genus-v Programming</i>, on [Youtube - Playlist](https://www.youtube.com/playlist?list=PLECOtlti4Psqw1qRaN4R9sWSQWvqfJU_V).
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+mod config;
+mod handler;
+
+use actix_web::{App,HttpServer,middleware::Logger};
+use color_eyre::Result;
+use crate::config::Config;
+use crate::handler::app_config;
+use tracing::info;
+
+#[actix_rt::main]
+async fn main() -> Result<()>{
+
+    let config = Config::from_env()
+        .expect("Server configuration");
+    
+    info!("Staring the server at http://{}:{}", config.host, config.port);
+
+    HttpServer::new(move ||{
+        App::new()
+        .wrap(Logger::default())
+        .configure(app_config)
+    })
+    .bind(format!("{}:{}", config.host, config.port))?
+    .run()
+    .await?;
+
+    Ok(())
+}
